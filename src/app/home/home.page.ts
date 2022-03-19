19fdf8353b65508816714f497a09649dab1071e1
Graham Naquin <grahamnaquin@Grahams-MacBook-Pro.local>
2022-03-19 15:41:32 -0500

    calendar page set up and a contact links added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,7 +18,7 @@ export class HomePage implements OnInit {
   contactButton() {
     console.log('contact button clicked');
     window.location.href =
-      'https://docs.google.com/forms/d/e/1FAIpQLSdWPif5D-Ige00RHWZ4w2t9YB-i9azPhQu17xgf--Ov4q8DAg/viewform?usp=sf_link';
+      'https://forms.gle/V3DSbQoPYMVzSxr78';
   };
 
 }
